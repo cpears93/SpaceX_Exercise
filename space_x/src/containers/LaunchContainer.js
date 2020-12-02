038c037f82aaed10a9a0dc1bef81775f763348f8
cpears93 <cpearson3140@gmail.com>
2020-12-02 17:52:14 +0000

    Initialised Containers.

---
+++ space_x/src/containers/LaunchContainer.js
@@ -0,0 +1,44 @@
+import React, { Component, Fragment
+} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LaunchList from '../components/launches/LaunchList';
+import Request from '../helpers/request.js';
+
+class LaunchContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            launches: []
+        }
+    }
+
+    componentDidMount() {
+        console.log("character container did mount")
+        const request = new Request();
+
+        request.get('https://api.spacexdata.com/v3/launches')
+            .then((data) => {
+                console.log(data);
+                this.setState({
+                    launches: data
+                })
+            })
+    }
+
+
+    render() {
+        if (!this.state.launch || this.state.launches.length === 0) {
+            return ( <h2> Loading </h2>)
+            }
+
+            return ( <
+                LaunchList launches = {
+                    this.state.launches
+                }
+                />
+            )
+        }
+
+    }
+
+    export default LaunchContainer;
