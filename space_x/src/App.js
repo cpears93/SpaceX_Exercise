a1dd1756ce16fcdb45872267cf3b47f71169688b
cpears93 <cpearson3140@gmail.com>
2020-12-02 17:22:23 +0000

    Removed boilerplate code in App.js.

--- space_x/src/App.js
+++ space_x/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Space X App</code>
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          A Comprehensive record of Space X launches
         </a>
       </header>
     </div>
